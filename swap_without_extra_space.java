2d7e85a1d15ec535dc150385bd44913595f8bd2f
HitJatin <adriano23012001@gmail.com>
2021-09-17 22:01:39 +0530

    added new function swap without extra space

---
+++ swap_without_extra_space.java
@@ -0,0 +1,7 @@
+public class swap_without_extra_space {
+    public void swapWithoutExtraSpace(int num1,int num2){
+        num1=num1+num2;
+        num2=num1-num2;
+        num1=num1-num2;
+    }
+}
