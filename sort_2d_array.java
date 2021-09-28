0f069ae4e7266c4e0d170a05f29f4d90ad27e30c
HitJatin <adriano23012001@gmail.com>
2021-09-28 21:16:00 +0530

    sort_2d_array

---
+++ sort_2d_array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sort_2d_array {
+    //Need to Review
+    public static  void Sort2DArrayBasedOnColumnNumber (int[][] array, final int columnNumber){
+        Arrays.sort(array, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] first, int[] second) {
+               if(first[columnNumber-1] > second[columnNumber-1]) return 1;
+               else return -1;
+            }
+        });
+    }
+}
